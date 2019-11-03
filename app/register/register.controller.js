b1529a0bb0b6878fd5f306d99e144aa8243d8630
Anudeep D <anudeepd@nbsotech.com>
2019-11-03 10:15:24 +0530

    register changes

--- app/register/register.controller.js
+++ app/register/register.controller.js
@@ -14,13 +14,18 @@
 
         function register(registerForm) {
             if (registerForm.$valid) {
+                vm.registerObj.role = 'FARMER';
                 UserService.register(vm.registerObj).then(function (data) {
-                    if (data && data.token) {
+                    if (data && data._id) {
                         noty.showSuccess("Register Success!! Please login");
                         $timeout(function () {
                             $state.go('login');
                         });
 
+                    }else if(data && data.errmsg){
+                        noty.showError(data.errmsg);
+                    }else{
+                        noty.showError("Something went wrong");    
                     }
                 }, function (error) {
                     console.log(error);
